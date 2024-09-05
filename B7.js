133a55aad754e79d94e95d916cd99eea10f68f28
Your Name <jasonbataillon@gmail.com>
2024-09-04 21:06:01 -0400

    Initial commit, also completed everything in this commit.

---
+++ B7.js
@@ -0,0 +1,13 @@
+//Here is setting start as the variable for the initial message talking about the vault secret code
+const start =
+  'You have received this message because you have been chosen to open an important vault. Here is the secret combination:';
+//Here is the first variable multiplying two numbers to create the first number of the code
+const var1 = 5 * 2;
+//Here is the second variable adding two numbers to create the second number of the code
+const var2 = 15 + 17;
+//Here is the third variable dividing two numbers to create the third number of the code
+const var3 = 156 / 2;
+//This last line of code is to create an alert popup on the page as it is loaded to show the initial
+//statement, followed by the three numbers constituting the safe combination.
+alert(start + ' ' + var1 + ' ' + var2 + ' ' + var3);
+console.log(start + ' ' + var1 + ' ' + var2 + ' ' + var3);
